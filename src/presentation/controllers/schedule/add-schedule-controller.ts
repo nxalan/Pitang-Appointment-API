7ea73c55a68bdb5cde7355dc8689f07b54db86e3
Alan <allan_net@live.com>
2022-04-09 19:35:55 -0300

    feat: ensure AddSchedule calls Validation with the correct values

--- src/presentation/controllers/schedule/add-schedule-controller.ts
+++ src/presentation/controllers/schedule/add-schedule-controller.ts
@@ -1,14 +1,19 @@
-import { HttpResponse, HttpRequest, Controller, AddSchedule } from './add-schedule-controller-protocols'
+import { HttpResponse, HttpRequest, Controller, AddSchedule, Validation } from './add-schedule-controller-protocols'
 import { badRequest, serverError, ok, forbidden } from '@/presentation/helpers/http/http-helper'
 import { NameInUseError } from '@/presentation/errors'
 
 export class AddScheduleController implements Controller {
   constructor (
-    private readonly addSchedule: AddSchedule
+    private readonly addSchedule: AddSchedule,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { name, birthday, scheduledDate } = httpRequest.body
       const schedule = await this.addSchedule.add({
         name,
