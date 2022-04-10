1756c7b57971d275dbc43e022dd0187e53bce448
Alan <allan_net@live.com>
2022-04-09 21:25:39 -0300

    feat: create a mongo helper to help on some tests

---
+++ src/infra/db/helpers/mongo-helper.ts
@@ -0,0 +1,36 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  url: null as unknown as string,
+
+  async connect (url: string): Promise<void> {
+    this.url = url
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+    this.client = null
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.url)
+    }
+    return this.client.db().collection(name)
+  },
+
+  map: (data: any): any => {
+    const { _id, ...collectionWithoutId } = data
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
+  }
+
+}
