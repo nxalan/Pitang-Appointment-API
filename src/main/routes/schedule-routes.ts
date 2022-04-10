5d421f77b8b492cd56eeeb317824592a2a9d69c9
Alan <allan_net@live.com>
2022-04-10 00:57:36 -0300

    feat: ensure POST /schedule returns 200 on add-schedule

--- src/main/routes/schedule-routes.ts
+++ src/main/routes/schedule-routes.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
+import { makeAddScheduleController } from '@/main/factories/controllers/schedule/add-schedule/add-schedule-controller-factory'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
 export default (router: Router): void => {
-  router.post('/schedule')
+  router.post('/schedule', adaptRoute(makeAddScheduleController()))
 }
