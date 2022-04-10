2e8dc884ccbb7ff52b5fe5032fa47587e8728cdb
Alan <allan_net@live.com>
2022-04-09 23:47:12 -0300

    chore: add express and server files

---
+++ src/main/config/app.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+export default app
--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,7 @@ import env from '@/main/config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = (await import('./config/app')).default
+    const app = (await import('@/main/config/app')).default
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
   .catch(console.error)
