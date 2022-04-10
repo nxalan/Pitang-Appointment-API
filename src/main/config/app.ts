c8b8f6755f8056d5f0662ae00b954bd07a445ec0
Alan <allan_net@live.com>
2022-04-09 23:52:34 -0300

    feat: add bodyParser middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import setupMiddlewares from './middlewares'
 import * as dotenv from 'dotenv'
 
 const app = express()
 dotenv.config()
+setupMiddlewares(app)
 export default app
