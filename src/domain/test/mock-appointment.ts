bfaf2a1fe8f408ef0a9dbab0fa5ec4bdeb1c1cb3
Alan <allan_net@live.com>
2022-04-12 00:09:20 -0300

    feat: ensure AddAppointment route returns 200 with a appointment_id

--- src/domain/test/mock-appointment.ts
+++ src/domain/test/mock-appointment.ts
@@ -20,5 +20,7 @@ export const mockAddAppointmentWithIdParams = (): AddAppointmentParams => ({
   appointment_id: 'any_id',
   name: 'any_name',
   birthday: new Date(new Date().setFullYear(new Date().getFullYear() - 20)),
-  appointment_date: new Date(new Date().setDate(new Date().getDate() + 1))
+  appointment_date: new Date(new Date().setDate(new Date().getDate() + 1)),
+  status: 'any_status',
+  status_comment: 'any_status_comment'
 })
