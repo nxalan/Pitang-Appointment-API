c044e0f7cfd5432b14693ddbd57e8a6f2bacc282
Alan <allan_net@live.com>
2022-04-11 22:30:29 -0300

    feat: ensure DbAddAppointment calls AddAppointmentRepository with correct values when id is provided

--- src/data/usecases/appointment/add-appointment/db-add-appointment.ts
+++ src/data/usecases/appointment/add-appointment/db-add-appointment.ts
@@ -1,18 +1,13 @@
-import { AddAppointment, AddAppointmentParams, AppointmentModel, AddAppointmentRepository, LoadAppointmentByNameRepository } from '.'
+import { AddAppointment, AddAppointmentParams, AppointmentModel, AddAppointmentRepository } from '.'
 
 export class DbAddAppointment implements AddAppointment {
   constructor (
-    private readonly addAppointmentRepository: AddAppointmentRepository,
-    private readonly loadAppointmentByNameRepository: LoadAppointmentByNameRepository
-  ) {}
+    private readonly addAppointmentRepository: AddAppointmentRepository
+  ) { }
 
   async add (appointmentData: AddAppointmentParams): Promise<AppointmentModel> {
-    const appointment = await this.loadAppointmentByNameRepository.loadByName(appointmentData.name)
-    if (!appointment) {
-      const newAppointment = await this.addAppointmentRepository.add(Object.assign(
-        {}, appointmentData, { status: 'NOT VACCINED', status_comment: '' }))
-      return newAppointment
-    }
-    return null as any
+    const newAppointment = await this.addAppointmentRepository.add(appointmentData.appointment_id ? appointmentData
+      : Object.assign({}, appointmentData, { status: 'NOT VACCINED', status_comment: '' }))
+    return newAppointment
   }
 }
