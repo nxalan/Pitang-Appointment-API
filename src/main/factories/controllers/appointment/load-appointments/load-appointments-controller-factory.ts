dcc58a97645492b141bccc9951f812fbb94dcf96
Alan <allan_net@live.com>
2022-04-14 20:55:29 -0300

    feat: ensure load-appointments-controller-composite call all methods

---
+++ src/main/factories/controllers/appointment/load-appointments/load-appointments-controller-factory.ts
@@ -0,0 +1,8 @@
+import { DbLoadAppointments } from '@/data/usecases/appointment/load-appointments/db-load-appointments'
+import { LoadAppointments } from '@/domain/usecases/appointment/load-appointments'
+import { AppointmentMongoRepository } from '@/infra/db/appointment/appointment-mongo-repository'
+
+export const makeDbLoadappointments = (): LoadAppointments => {
+  const appointmentMongoRepository = new AppointmentMongoRepository()
+  return new DbLoadAppointments(appointmentMongoRepository)
+}
