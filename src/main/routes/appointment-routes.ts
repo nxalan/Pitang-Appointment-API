581e0ddcbcb8fa9addeb3d66864b5460ea0fd052
Alan <allan_net@live.com>
2022-04-15 20:51:44 -0300

    feat: ensure GET appointment/restricted-dates route returns a list of restricted dates

--- src/main/routes/appointment-routes.ts
+++ src/main/routes/appointment-routes.ts
@@ -5,10 +5,13 @@ import { makeEditAppointmentController } from '@/main/factories/controllers/appo
 import { makeLoadAppointmentsController } from '@/main/factories/controllers/appointment/load-appointments/load-appointments-controller-factory'
 import { makeDeleteAppointmentController } from '@/main/factories/controllers/appointment/delete-appointment/delete-appointment-controller-factory'
 import { makeLoadAppointmentByIdController } from '@/main/factories/controllers/appointment/load-appointment-by-id/load-appointment-by-id-controller-factory'
+import { makeLoadRestrictedDatesController } from '@/main/factories/controllers/appointment/load-restricted-dates/load-restricted-dates-controller-factory'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
+
 export default (router: Router): void => {
   router.get('/appointments', adaptRoute(makeLoadAppointmentsController()))
   router.get('/appointment/:id', adaptRoute(makeLoadAppointmentByIdController()))
+  router.get('/appointments/restricted-dates', adaptRoute(makeLoadRestrictedDatesController()))
   router.post('/appointment/', adaptRoute(makeAddAppointmentController()))
   router.put('/appointment/:id', adaptRoute(makeEditAppointmentController()))
   router.delete('/appointment/:id', adaptRoute(makeDeleteAppointmentController()))
