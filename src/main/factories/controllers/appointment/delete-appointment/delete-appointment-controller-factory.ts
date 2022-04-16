199d2b64153abcc9e7482b2061cb317ba0e7cc7e
Alan <allan_net@live.com>
2022-04-15 22:38:58 -0300

    feat: ensure id validation validates deleteAppointmentController

--- src/main/factories/controllers/appointment/delete-appointment/delete-appointment-controller-factory.ts
+++ src/main/factories/controllers/appointment/delete-appointment/delete-appointment-controller-factory.ts
@@ -1,5 +1,6 @@
 import { DeleteAppointmentController } from '@/presentation/controllers/appointment/delete-appointment/delete-appointment-controller'
 import { Controller } from '@/presentation/protocols'
+import { makeDeleteAppointmentValidation } from './delete-appointment-validation-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbDeleteAppointment } from '@/main/factories/usecases/appointment/delete-appointment/db-delete-appointment-factory'
 import { makeDbLoadAppointmentById } from '@/main/factories/usecases/appointment/load-appointment-by-id/db-load-appointment-by-id-factory'
@@ -7,6 +8,7 @@ import { makeDbLoadAppointmentById } from '@/main/factories/usecases/appointment
 export const makeDeleteAppointmentController = (): Controller => {
   const controller = new DeleteAppointmentController(
     makeDbDeleteAppointment(),
+    makeDeleteAppointmentValidation(),
     makeDbLoadAppointmentById()
   )
   return makeLogControllerDecorator(controller)
